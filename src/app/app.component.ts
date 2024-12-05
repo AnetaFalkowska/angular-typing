cc84e112ba83e93e31f71964fec9f82386ddf10c
Aneta Falkowska <aneta.falkowska@gmail.com>
2024-12-05 22:02:49 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-typing';
+  randomText = faker.lorem.word();
+  enteredText = '';
+  // letters: string[] = []
+
+  // letterStyle(index:number) {
+  //   if (this.letters[index])
+  //     {return {'color': this.letters[index]}}
+  //   return {}
+  // }
+
+  // onInput(value: string) {
+  //   this.enteredText = value;
+  //   this.letters = []
+  //   for (let i = 0; i<this.enteredText.length; i++) {
+  //     if (this.enteredText[i]===this.randomText[i]) {
+  //       this.letters.push("green")
+  //     } else (this.letters.push("red"))
+  //   }
+  // }
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
